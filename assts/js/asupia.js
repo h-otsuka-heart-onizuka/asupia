4455f0d16b15f0c7a59ddff02f06df240f1c2685
otsuka-heart <h-otsuka@heart.onizuka.co.jp>
2026-03-04 16:15:39 +0900

    JSに重要なお知らせの表示制御を記述（動くファイル）

--- assts/js/asupia.js
+++ assts/js/asupia.js
@@ -1,9 +1,10 @@
-function getID() {
+function checkImportantNews() {
   const importantNewsWrapper = document.querySelector('#vkexunit_post_list-3');
   if (!importantNewsWrapper) return;
 
   const newsList = importantNewsWrapper.querySelector('ul');
   if (!newsList) {
     importantNewsWrapper.style.display = "none";
-  } 
+  }
 }
+document.addEventListener('DOMContentLoaded', checkImportantNews);
