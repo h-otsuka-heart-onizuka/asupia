7dd1c1c5ec87283324605fabac928bdc4d3dfdee
otsuka-heart <h-otsuka@heart.onizuka.co.jp>
2026-03-04 15:41:44 +0900

    JSの不要な記述を削除

--- assts/js/asupia.js
+++ assts/js/asupia.js
@@ -1,8 +1,7 @@
-console.log('JS表示');
 function getID() {
   const importantNewsWrapper = document.querySelector('#vkexunit_post_list-3');
   if (importantNewsWrapper) return;
-  
+
   const newsList = importantNewsWrapper.querySelector('ul');
   if (!newsList) {
     importantNewsWrapper.style.display = "none";
